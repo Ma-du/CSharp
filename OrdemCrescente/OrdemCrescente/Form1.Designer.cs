30bdb616a18e930b0736cb93de4c4ddfa41fb8dd
Maria Eduarda Nunes <80704219+Ma-du@users.noreply.github.com>
2021-03-15 13:28:33 -0300

    Add files via upload

---
+++ OrdemCrescente/OrdemCrescente/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace OrdemCrescente
+{
+    partial class frmOrdemCrescente
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblNum3 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(21, 8);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(60, 13);
+            this.lblNum1.TabIndex = 0;
+            this.lblNum1.Text = "1º Número:";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(141, 9);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(60, 13);
+            this.lblNum2.TabIndex = 1;
+            this.lblNum2.Text = "2º Número:";
+            // 
+            // lblNum3
+            // 
+            this.lblNum3.AutoSize = true;
+            this.lblNum3.Location = new System.Drawing.Point(260, 8);
+            this.lblNum3.Name = "lblNum3";
+            this.lblNum3.Size = new System.Drawing.Size(60, 13);
+            this.lblNum3.TabIndex = 2;
+            this.lblNum3.Text = "3º Número:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(3, 25);
+            this.txtNum1.Multiline = true;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 39);
+            this.txtNum1.TabIndex = 3;
+            this.txtNum1.Text = "0";
+            this.txtNum1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(122, 25);
+            this.txtNum2.Multiline = true;
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 39);
+            this.txtNum2.TabIndex = 4;
+            this.txtNum2.Text = "0";
+            this.txtNum2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum3.Location = new System.Drawing.Point(246, 25);
+            this.txtNum3.Multiline = true;
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(100, 39);
+            this.txtNum3.TabIndex = 5;
+            this.txtNum3.Text = "0";
+            this.txtNum3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenar.ForeColor = System.Drawing.Color.Green;
+            this.btnOrdenar.Location = new System.Drawing.Point(3, 70);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(343, 38);
+            this.btnOrdenar.TabIndex = 6;
+            this.btnOrdenar.Text = "ORDENAR";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // frmOrdemCrescente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(351, 124);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum3);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Name = "frmOrdemCrescente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ordem Crescente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblNum3;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.Button btnOrdenar;
+    }
+}
+
---
+++ OrdemCrescente/OrdemCrescente/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrdemCrescente
+{
+    public partial class frmOrdemCrescente : Form
+    {
+        public frmOrdemCrescente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            int n1 = int.Parse(txtNum1.Text);
+            int n2 = int.Parse(txtNum2.Text);
+            int n3 = int.Parse(txtNum3.Text);
+
+            if (n1 == n2 || n1 == n3 || n2 == n3)
+            {
+                MessageBox.Show("ATENÇÃO. Os número devem ser distintos.");
+            }
+            else if (n1 < n2 && n2 < n3)
+            {
+                MessageBox.Show(n1 + " - " + n2 + " - " + n3);
+            }
+            else if (n1 < n3 && n3 < n2)
+            {
+                MessageBox.Show(n1 + " - " + n3 + " - " + n2);
+            }
+            else if (n2 < n1 && n1 < n3)
+            {
+                MessageBox.Show(n2 + " - " + n1 + " - " + n3);
+            }
+            else if (n2 < n3 && n3 < n1)
+            {
+                MessageBox.Show(n2 + " - " + n3 + " - " + n1);
+            }
+            else if (n3 < n1 && n1 < n2)
+            {
+                MessageBox.Show(n3 + " - " + n1 + " - " + n2);
+            }
+            else if (n3 < n2 && n2 < n1)
+            {
+                MessageBox.Show(n3 + " - " + n2 + " - " + n1);
+            }
+        }
+    }
+}
